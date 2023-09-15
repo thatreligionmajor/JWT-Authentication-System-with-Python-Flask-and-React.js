7972c286130383531ffd5a9378ef2fc2fe4c766d
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-09-15 20:22:27 +0000

    private page not loading, almost there

---
+++ src/front/js/pages/private.js
@@ -0,0 +1,26 @@
+import React, { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
+
+
+const Private = () => {
+    const { store, actions } = useContext(Context);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (store.token && store.token !== "" && store.token !== undefined ?
+            navigate("/private")
+
+            : navigate("/login"));
+
+    }, [store.token]);
+
+    return (
+        <div className="text-center mt-5">
+            <h1>Welcome to the hall of mysteries!</h1>
+            <h2>Only those with the proper key can see this area!</h2>
+            <h3>Here lie the esoteric secrets.</h3>
+        </div>
+    )
+}
+export default Private;
